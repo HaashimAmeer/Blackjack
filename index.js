ac25ee2d97a0d31450e1be99aa11aeb18c2a1f4e
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2021-12-19 11:45:52 -0800

    Added addition logic & main styling

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum === 21) {
-  hasBlackJack = true;
-  message = "Congratulations, you've got Blackjack! 🥳";
-} else if (sum < 21) {
-  message = "Do you want to draw a new card? 🙃";
-} else {
-  isAlive = false;
-  message = "Tough luck! You're out of the game. 😭";
+function startGame() {
+  if (sum === 21) {
+    hasBlackJack = true;
+    message = "Congratulations, you've got Blackjack! 🥳";
+  } else if (sum < 21) {
+    message = "Do you want to draw a new card? 🙃";
+  } else {
+    isAlive = false;
+    message = "Tough luck! You're out of the game. 😭";
+  }
+
+  console.log(message);
 }
