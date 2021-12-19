5a0c31680cfe51d58e7173227f5a0ac7ee09633e
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2021-12-19 14:11:12 -0800

    Added player object & reformatted.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+  name: "Bob",
+  chips: 25,
+};
 let firstCard;
 let secondCard;
 let cards = [];
@@ -6,10 +10,13 @@ let hasBlackJack;
 let isAlive;
 let message = "";
 
+let playerEl = document.querySelector("#player-el");
 let messageEl = document.querySelector("#message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 let newEl = document.querySelector("#new-btn");
+
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function startGame() {
   hasBlackJack = false;
